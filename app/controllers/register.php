e0ef67d3b1f725b2333a57cce7a327ebafb95c68
mehraneh12 <168213183+mehraneh12@users.noreply.github.com>
2024-05-09 17:54:14 +0330

    fix issue

--- app/controllers/register.php
+++ app/controllers/register.php
@@ -1,25 +1,4 @@
 <?php
-<<<<<<< HEAD
-class register extends controller{
-    public $checklogin='';
- function __construct() {
-    parent::__construct();
-    if($this->checklogin != FALSE){
-        header("Location:".URL);
-    }
-}
-
-function index(){
-    $this->view(viewUrl:'register/index');
-}
-
-function insert_data(){
-    $this->model->insert($_POST);
-}
-}
-?>
-=======
-
 class register extends  Controller
 {
     public $checkLogin = '';
@@ -42,4 +21,3 @@ class register extends  Controller
     }
 
 }
->>>>>>> d681531b2d83c682e208a6083c89acb2e9199b95
--- core/config.php
+++ core/config.php
@@ -2,12 +2,7 @@
 //Database config
 const USERNAME = "root";
 const PASSWORD = "";
-<<<<<<< HEAD
-const DATABASE = "test";
-const SERVER_NAME = "localhost";
-=======
 const DATABASE = "faravinmessenger";
 const SERVER_NAME = "localhost";
 const URL = "localhost/faravin";
->>>>>>> b45b36b (first Initial)
 ?>
